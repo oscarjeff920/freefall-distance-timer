26f97174665653f6f91653af7910067922847f92
oscarjeff920 <65213748+oscarjeff920@users.noreply.github.com>
2021-08-25 09:20:18 -0300

    added continue button on timer

---
+++ lib/presentation/widgets/timer_buttons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TimerButtons extends StatelessWidget {
+  const TimerButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      ),
+    );
+  }
+}
