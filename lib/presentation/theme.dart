e3f802c7cb005af8d97365a0d0e6eed1895c4dbd
oscarjeff920 <65213748+oscarjeff920@users.noreply.github.com>
2021-09-06 15:14:39 -0300

    updated ui elements including changing theme and display font and colours

--- lib/presentation/theme.dart
+++ lib/presentation/theme.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData standardTheme = ThemeData(
-  fontFamily: 'Raleway',
-  scaffoldBackgroundColor: Colors.white30,
+  fontFamily: 'Play',
+  scaffoldBackgroundColor: Color(0xFF5A5F64),
   appBarTheme: AppBarTheme(
     color: Colors.amber.withAlpha(500),
     textTheme: TextTheme(
       headline6: TextStyle(
-        color: Colors.black26.withOpacity(0.8),
+        color: Colors.black26.withOpacity(1.0),
         fontSize: 25.0,
         fontWeight: FontWeight.bold,
         letterSpacing: 2.0
